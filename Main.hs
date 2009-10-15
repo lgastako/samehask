2fe8913a711fe3796aaa757a0d6723b38c225a5f
John Evans <john@milo.com>
2009-10-14 23:16:18 -0700

    Got initial set of balls displaying.

--- Main.hs
+++ Main.hs
@@ -9,15 +9,18 @@ import System.IO.Unsafe
 import Graphics.UI.SDL.Image as Image
 import Graphics.UI.SDL as SDL
 
+type Board = Array (Int, Int) Int
+type Game  = (Board, [Surface])
+
 -- "Constants"
 num_balls :: Int
 num_balls = 3
 
 h :: Int
-h = 10
+h = 20
 
 w :: Int
-w  = 10
+w  = 20
 
 bw :: Int
 bw = 28
@@ -43,20 +46,14 @@ apply_surface x y src dst =
 
 
 random_ball :: IO Int
-random_ball = (getStdRandom . randomR) (1,3)
+random_ball = (getStdRandom . randomR) (0,(dec num_balls))
 
 -- Lame.  TODO: Do away with this hack.
 random_ball_xy :: Int -> Int -> Int
 random_ball_xy x y = unsafePerformIO random_ball
 
--- generate_random_board :: IO (Array (Int, Int) Int)
--- generate_random_board =
---     do
---       array (w, h) [((x, y), n) | n <- random_ball,
---                                   x <- range (0, w - 1),
---                                   y <- range (0, h - 1)]
 
---new_board ::
+new_board :: Board
 new_board =
       array ((0, 0), (w, h)) [((x, y), rxy x y) | x <- range (0, w - 1),
                                                   y <- range (0, h - 1)]
@@ -66,6 +63,32 @@ load_ball :: Int -> IO Surface
 load_ball n = load_image ("images/ball-" ++ (show n) ++ ".png")
 
 
+-- TODO: Really?
+dec :: Int -> Int
+dec n = n - 1
+
+draw_cell :: Game -> Surface -> Int -> Int -> IO Bool
+draw_cell (b, balls) s y x =
+      apply_surface (x * bw) (y * bw) (balls !! (b ! (x, y))) s
+
+
+draw_down_cols :: Game -> Surface -> Int -> Int -> IO Bool
+draw_down_cols g s y x
+    | x > 0 = draw_cell g s y x >> draw_down_cols g s y (dec x)
+    | otherwise = draw_cell g s y x
+
+
+draw_down_rows :: Game -> Surface -> Int -> IO Bool
+draw_down_rows g s y
+    | y > 0 = draw_down_cols g s y (dec w) >> draw_down_rows g s (dec y)
+    | otherwise = draw_down_cols g s y (dec w)
+
+
+draw_board :: Game -> Surface -> IO Bool
+draw_board g s =
+    draw_down_rows g s (dec h)
+
+
 main :: IO ()
 main =
     do
@@ -73,14 +96,12 @@ main =
       screen <- setVideoMode screen_width screen_height screen_bpp []
       setCaption "SameHask" []
       balls <- mapM load_ball [1..3]
-      apply_surface 0 0 (head balls) screen
-      apply_surface bw 0 (head (tail balls)) screen
-      apply_surface 0 bw (head (tail (tail balls))) screen
+      draw_board (board, balls) screen
       SDL.flip screen
       quitHandler
     where
-      screen_width  = 640
-      screen_height = 480
+      screen_width  = w * bw
+      screen_height = h * bw
       screen_bpp    = 32
       std_gen       = mkStdGen 1
       board         = new_board
