2ca86c3b2cc1b57d4decb3a2d0e69fc09db8c05d
John Evans <john@milo.com>
2009-10-23 21:15:06 -0700

    Added empty space image and eliminated unsafePerformIO

--- Main.hs
+++ Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Random
 import Control.Monad
-import System.IO.Unsafe
 
 import Graphics.UI.SDL.Image as Image
 import Graphics.UI.SDL as SDL
@@ -10,6 +9,7 @@ import Graphics.UI.SDL as SDL
 type Board = [[Int]]
 type Game  = (Board, [Surface])
 type Point = (Int, Int)
+type Ball  = Int
 
 -- Gratuitous?
 type Filename = String
@@ -48,16 +48,14 @@ apply_surface x y src dst =
 
 
 random_ball :: IO Int
-random_ball = (getStdRandom . randomR) (0,(dec num_balls))
+random_ball = (getStdRandom . randomR) (1,num_balls)
 
 
--- Lame.  TODO: Do away with this hack.
-random_ball_xy :: Int -> Int -> Int
-random_ball_xy _ _ = unsafePerformIO random_ball
+new_row :: IO [Ball]
+new_row = forM [0..w] (\_ -> random_ball)
 
-
-new_board :: Board
-new_board = [ [ random_ball_xy x y | x <- [0..w]] | y <- [0..h] ]
+new_board :: IO Board
+new_board = forM [0..h] (\_ -> new_row)
 
 
 load_ball :: Int -> IO Surface
@@ -91,52 +89,90 @@ draw_board g s =
     draw_down_rows g s (dec h)
 
 
-build_eradication_list :: Board -> Point -> Int -> [Point] -> [Point]
-build_eradication_list b (x, y) v accum
-    | (x < 0) || (y < 0)  = accum
-    | (x > w) || (y > h)  = accum
-    | (x, y) `elem` accum = accum
-    | otherwise           =
-        build_eradication_list b ((pred x), y) v accum3
-        where
-          accum3 = build_eradication_list b ((succ x), y) v accum2
-          accum2 = build_eradication_list b (x, (pred y)) v accum1
-          accum1 = build_eradication_list b (x, (succ y)) v accum
+-- build_eradication_list :: Board -> Point -> Int -> [Point] -> [Point]
+-- build_eradication_list b pt v accum
+--     | (x < 0) || (y < 0)  = accum
+--     | (x > w) || (y > h)  = accum
+--     | (x, y) `elem` accum = accum
+--     | otherwise           =
+--         build_eradication_list b ((pred x), y) v accum3
+--         where
+--           accum3 = build_eradication_list b ((succ x), y) v accum2
+--           accum2 = build_eradication_list b (x, (pred y)) v accum1
+--           accum1 = build_eradication_list b (x, (succ y)) v (pt:accum)
+--           (x, y) = pt
 
 
--- From here:
--- http://hackage.haskell.org/packages/archive/luhn/0.1/doc/html/src/Luhn.html
--- Like Python's enumerate function - returns a tuple where the first
--- element is the index from 0 of the second element in the input list.
-enumerate :: Integral n => [a] -> [(n, a)]
-enumerate xs = enumerate' 0 xs
-    where
-        enumerate' _ [] = []
-        enumerate' counter (a:as) =
-            (counter, a) : enumerate' (counter + 1) as
+-- -- From here:
+-- -- http://hackage.haskell.org/packages/archive/luhn/0.1/doc/html/src/Luhn.html
+-- -- Like Python's enumerate function - returns a tuple where the first
+-- -- element is the index from 0 of the second element in the input list.
+-- enumerate :: Integral n => [a] -> [(n, a)]
+-- enumerate xs = enumerate' 0 xs
+--     where
+--         enumerate' _ [] = []
+--         enumerate' counter (a:as) =
+--             (counter, a) : enumerate' (counter + 1) as
 
 
-eradicate :: Board -> [Point] -> Board
-eradicate b []     = b
-eradicate b points =
-   [handle_row y row | (y, row) <- enumerate b]
-   where
-     handle_row y row =
-         [handle_col y x v | (x, v) <- enumerate row]
-     handle_col y x v
-         | (x, y) `elem` points = 0
-         | otherwise            = v
+-- eradicate :: Board -> [Point] -> Board
+-- eradicate b []     = b
+-- eradicate b points =
+--    [handle_row y row | (y, row) <- enumerate b]
+--    where
+--      handle_row y row =
+--          [handle_col y x v | (x, v) <- enumerate row]
+--      handle_col y x v
+--          | (x, y) `elem` points = 0
+--          | otherwise            = v
 
 
 get_ball :: Board -> Point -> Int
 get_ball b (x, y) = (b !! y) !! x
 
 
-remove_adjacent :: Board -> Point -> Board
-remove_adjacent b xy =
-    eradicate b eradication_list
-    where eradication_list = build_eradication_list b xy value []
-          value = get_ball b xy
+-- remove_adjacent :: Board -> Point -> Board
+-- remove_adjacent b xy =
+--     eradicate b eradication_list
+--     where eradication_list = build_eradication_list b xy value []
+--           value = get_ball b xy
+
+point_u :: Point -> Point
+point_u (x, y) = (x, y - 1)
+
+point_d :: Point -> Point
+point_d (x, y) = (x, y + 1)
+
+point_l :: Point -> Point
+point_l (x, y) = (x - 1, y)
+
+point_r :: Point -> Point
+point_r (x, y) = (x + 1, y)
+
+
+eradicate :: Board -> Point -> Int -> Board
+eradicate board point ball
+    | out_of_bounds point = board
+    | wrong_color (get_ball board point) ball = board
+    | otherwise =
+        (eradicate
+         (eradicate
+          (eradicate
+           (eradicate
+            board
+            (point_u point) ball)
+           (point_d point) ball)
+          (point_l point) ball)
+         (point_r point) ball)
+        where wrong_color = (/=)
+              out_of_bounds (x,y)
+                  | x < 0 = True
+                  | y < 0 = True
+                  | x > w = True
+                  | y > h = True
+                  | otherwise = False
+
+
 
 mouse_point_to_board_point :: Point -> Point
 mouse_point_to_board_point (x, y) = (x `div` w, y `div` h)
@@ -144,7 +180,10 @@ mouse_point_to_board_point (x, y) = (x `div` w, y `div` h)
 
 remove_from_mouse_click :: Board -> Point -> Board
 remove_from_mouse_click b click =
-    remove_adjacent b $ mouse_point_to_board_point click
+    eradicate b location ball
+    where
+      ball  = get_ball b location
+      location = mouse_point_to_board_point click
 
 
 main :: IO ()
@@ -153,7 +192,9 @@ main =
       SDL.init [InitEverything]
       screen <- setVideoMode screen_width screen_height screen_bpp []
       setCaption "SameHask" []
-      balls <- mapM load_ball [1..3]
+      balls <- mapM load_ball [0..3]
+      board <- new_board
+--      draw_board ((remove_from_mouse_click board (0, 0)), balls) screen
       draw_board (board, balls) screen
       SDL.flip screen
       quitHandler
@@ -162,7 +203,6 @@ main =
       screen_height = h * bw
       screen_bpp    = 32
       std_gen       = mkStdGen 1
-      board         = new_board
 
 
 
